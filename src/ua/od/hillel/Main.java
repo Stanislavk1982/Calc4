b44422f6dbbf6f52ea6394edf70ec80d2f1523af
Stanislav Kvashchuk <StanislavK.1982@gmail.com>
2016-03-30 15:17:09 +0300

    Commit2 Calc4

--- src/ua/od/hillel/Main.java
+++ src/ua/od/hillel/Main.java
@@ -6,23 +6,23 @@ public class Main {
         // write your code here
         int number1 = 0, number2 = 0, result = 0;
         char operation;
-        String string;
-        string = args[0];
+        String string= args[0];
+        //string = args[0];
         //operation = string.charAt(0);
-        operation = args[0].charAt(0);
+        //operation = args[0].charAt(0);
         number1 = Integer.parseInt(args[1]);
         number2 = Integer.parseInt(args[2]);
-        switch (operation) {
-            case '+':
+        switch (string) {
+            case "+":
                 result=number1+number2;
                 break;
-            case '-':
+            case "-":
                 result=number1-number2;
                 break;
-            case '*':
+            case "*":
                 result=number1*number2;
                 break;
-            case '/':
+            case "/":
                 result=number1/number2;
                 break;
         }
