cedc691a505a54c14216dc253b20890e4458b94d
Stanislav Kvashchuk <StanislavK.1982@gmail.com>
2016-03-30 22:20:40 +0300

    commit 3 Calc4

--- src/ua/od/hillel/Calc3_01.java
+++ src/ua/od/hillel/Calc3_01.java
@@ -1,17 +1,17 @@
 package ua.od.hillel;
 
-public class Main {
+public class Calc3_01 {
 
     public static void main(String[] args) {
         // write your code here
         int number1 = 0, number2 = 0, result = 0;
         char operation;
-        String string= args[0];
+        String string= args[2];
         //string = args[0];
         //operation = string.charAt(0);
         //operation = args[0].charAt(0);
-        number1 = Integer.parseInt(args[1]);
-        number2 = Integer.parseInt(args[2]);
+        number1 = Integer.parseInt(args[0]);
+        number2 = Integer.parseInt(args[1]);
         switch (string) {
             case "+":
                 result=number1+number2;
@@ -26,6 +26,7 @@ public class Main {
                 result=number1/number2;
                 break;
         }
+        System.out.println(string);
         System.out.println("Result: " +result);
 
         //c= args.;
